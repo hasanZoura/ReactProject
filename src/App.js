18e4c8acf17b681bcfd163f012ba096613820b18
Ilya Zoura <ilyazoura1@gmail.com>
2024-04-08 06:08:01 +0330

    Some changes...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const owner = {
+    name: 'Mr. Web',
+    lastName: 'Developer',
+    age: 1001011,
+    career: 'Web developer'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello World I'm {owner.name}</h1>
+    </>
   );
 }
 
